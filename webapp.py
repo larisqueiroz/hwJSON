4fd2613b4e9f7db03b338945235c8f6c24c602ea
larissa <larissa.queiroz@lds.ifce.edu.br>
2019-10-24 15:20:59 -0300

    primeiras modificacoes

--- webapp.py
+++ webapp.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, session, json
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session, json, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore, login_required, UserMixin, RoleMixin
 from flask_security.utils import hash_password
+from werkzeug.security import check_password_hash
 import subprocess
+from flask_login import login_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -45,24 +47,41 @@ class Role(db.Model, RoleMixin):
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
-
-@app.route('/listarhw') #### parte do json
-@login_required
-def listar():
-    return  jsonify(new_text), 200
-
 @app.route('/cadastrar', methods=['POST', 'GET'])
 def cadastrar():
     if request.method == 'POST':
         user_datastore.create_user(
             email= request.form.get('email'),
-            password = hash_password(request.form.get('password'))
+            password = request.form.get('password')#hash_password(request.form.get('password'))
         )
         db.session.commit()
 
-        return redirect(url_for('listarhw'))
+        return redirect(url_for('login_post'))
 
     return render_template('cadastrar.html')
+
+@app.route('/entrar', methods=['POST', 'GET'])
+def login_post():
+    if request.method == 'POST':
+        email = request.form.get('email') #login
+        passw = request.form.get('password')#login
+
+        user = User.query.filter_by(email=email).first()#db
+        print(user.password)
+        print(email)
+        print(passw)
+        print(user.email)
+
+        if user.email != email and user.password != passw:
+            print("entrou")
+            return "<strong>Erro. Tente novamente.</strong>"
+
+        if user.email == email and user.password == passw:
+            return  jsonify(new_text), 200
+        else:
+            return  "<strong>Erro. Tente novamente.</strong>"
+
+    return render_template('perfil.html')
 
 
 if __name__ == '__main__':
